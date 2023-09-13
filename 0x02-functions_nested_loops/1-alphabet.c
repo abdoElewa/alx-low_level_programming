2f02ad857b86d2ac7b9966c7679fbf56facff08e
abdoElewa <abdoelewa246810@gmail.com>
2023-09-13 09:10:24 -0700

    1-alphabet.c done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
- * Return: Always 0.
+ * print_alphabet_x10 - print the alphabest 10 times
+ * Return: void here
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+		while (i <= 9)
+		{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+			_putchar('\n');
+			i++;
+		}
+}
